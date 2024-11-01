3990b8dc42dc4291ea13d4fa09a917cd36dc3d55
Samuel Lanzuela <744726@student.dusd.net>
2024-11-01 10:00:26 -0700

    WORKED ON PROJECT
    Today i added rigidbodies to my perhabs

--- 2DBeginnerAdventureP2Williams/Assets/Scripts/DuckoController.cs
+++ 2DBeginnerAdventureP2Williams/Assets/Scripts/DuckoController.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class DuckoController : MonoBehaviour
 {
-    public float speed = 4.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
-        
-        transform.position = position;
+
+        void FixedUpdate()
+        {
+            Vector2 position = transform.position;
+            position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+            position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+            rigidbody2d.MovePosition(position);
+        }
     }
-}
+
