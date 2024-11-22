d0795dc3d698e1d9888e555eb9e420efd6b29835
Samuel Lanzuela <744726@student.dusd.net>
2024-11-22 10:00:44 -0800

    did 2dadventure
    Finshed animations and organizied

--- 2DBeginnerAdventureP2Williams/Assets/Scripts/DuckoController.cs
+++ 2DBeginnerAdventureP2Williams/Assets/Scripts/DuckoController.cs
@@ -20,13 +20,21 @@ public class DuckoController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-       
+       animator = GetComponent<Animator>();
+
+
+
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -34,6 +42,18 @@ public class DuckoController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look X", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+      
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
